1b33e29f72154ec2cde3e144261373f5a2868301
Rickard Andersson <gonz@severnatazvezda.com>
2016-03-14 01:18:51 +0200

    Redo store to use list of locs

--- lib/kotl/name_store.ex
+++ lib/kotl/name_store.ex
@@ -14,7 +14,7 @@ defmodule KOTL.NameStore do
   # API #
   #######
 
-  def start_link(names \\ Application.get_env(:kotl, :names),
+  def start_link(names \\ _init_locations(Application.get_env(:kotl, :names)),
                  opts \\ [name: __MODULE__]) do
     GenServer.start_link(__MODULE__, names, opts)
   end
@@ -53,33 +53,55 @@ defmodule KOTL.NameStore do
     GenServer.cast(pid, {:remove, spec})
   end
 
-  @spec names :: map
+  @spec names :: [Location.t]
   def names, do: names(__MODULE__)
 
-  @spec names(pid) :: map
+  @spec names(pid) :: [Location.t]
   def names(pid) do
     GenServer.call(pid, :names)
+  end
+
+  @spec search(:name, atom | String.t) :: [Location.t]
+  def search(:name, name) do
+    search(__MODULE__, :name, name)
+  end
+
+  @spec search(pid, :name, atom | String.t) :: [Location.t]
+  def search(pid, :name, name) do
+    GenServer.call(pid, {:search, :name, name})
   end
 
   ############
   # Internal #
   ############
 
+  defp _init_locations(locs) do
+    Enum.map(locs,
+      fn %{type: type, name: name, location: location} ->
+        %Location{type: type, name: name, location: location}
+        end)
+  end
+
   def init(names) do
     {:ok, names}
   end
 
   def handle_cast({:add, spec}, names) do
-    new_names = Map.put(names, {spec.type, spec.name}, spec.location)
+    new_names = [spec | names]
     {:noreply, new_names}
   end
 
   def handle_cast({:remove, spec}, names) do
-    new_names = Map.delete(names, {spec.type, spec.name})
+    new_names = Enum.reject(names, &(&1 == spec))
     {:noreply, new_names}
   end
 
   def handle_call(:names, _from, names) do
     {:reply, names, names}
+  end
+
+  def handle_call({:search, :name, name}, _from, names) do
+    found_names = Enum.filter(names, &(&1.name == name))
+    {:reply, found_names, names}
   end
 end
