b4e0e7f9a23feb7dd77e839f0fdab7804e656c9e
Rickard Andersson <gonz@severnatazvezda.com>
2016-03-15 00:55:38 +0200

    Specify location to be anything
    Not just atom | String.t

--- lib/kotl/location.ex
+++ lib/kotl/location.ex
@@ -2,5 +2,5 @@ defmodule KOTL.Location do
   defstruct [:type, :name, :location]
   @type t :: %KOTL.Location{type: atom,
                             name: (atom | String.t),
-                            location: (atom | String.t)}
+                            location: any}
 end
