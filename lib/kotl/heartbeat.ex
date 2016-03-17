6b574c419cad4210fdf49c1673b2bf603293b38e
Rickard Andersson <gonz@severnatazvezda.com>
2016-03-17 08:38:29 +0200

    Add moduledocs to structs
    Explaining what they are for and how they are structured

--- lib/kotl/heartbeat.ex
+++ lib/kotl/heartbeat.ex
@@ -1,5 +1,10 @@
 defmodule KOTL.Heartbeat do
+  @moduledoc"""
+  Stores a datetime and a result. The result is a positive integer representing
+  the time it took to get a response from a server or the like, meaning for
+  example a ping to a hostname to see if it's up or not.
+  """
   @type t :: %KOTL.Heartbeat{datetime: Timex.DateTime.t,
-                             response: pos_integer}
+                             response: integer}
   defstruct [:datetime, :response]
 end
--- lib/kotl/monitoree.ex
+++ lib/kotl/monitoree.ex
@@ -1,4 +1,13 @@
 defmodule KOTL.Monitoree do
+  @moduledoc"""
+  Stores information about a monitoree, which is a unit representing something
+  that will have some kind of health/presence check done on it. This can be,
+  for example, a host that will be pinged regularly or a process/node that will
+  be pinged in much the same way.
+
+  It embeds a list of heartbeats (results comprised of datetime + response time)
+  to store the historical data.
+  """
   @type t :: %KOTL.Monitoree{location: KOTL.Location.t,
                              data: [KOTL.Heartbeat.t]}
   defstruct [:location, :data]
