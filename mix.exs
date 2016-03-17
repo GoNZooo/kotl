2e427d249780e1b8c45ac3d368bf574adbb6fa49
Rickard Andersson <gonz@severnatazvezda.com>
2016-03-17 08:40:14 +0200

    Add Timex as dependency

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule KOTL.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger],
+    [applications: [:tzdata, :logger],
      mod: {KOTL, []}]
   end
 
@@ -28,6 +28,6 @@ defmodule KOTL.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:timex, "~> 2.1"}]
   end
 end
