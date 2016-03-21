75e8b82340e5b5722daf5836999fb136fbbc3d78
Rickard Andersson <gonz@severnatazvezda.com>
2016-03-21 22:19:21 +0200

    Make Monitor the Task
    Will use Monitor.{Supervisor,Manager} to be started, etc.; Supervised tasks

--- lib/kotl/monitor.ex
+++ lib/kotl/monitor.ex
@@ -1,58 +1,16 @@
 defmodule KOTL.Monitor do
-  use GenServer
-
-  @todo"""
-  Integrate result storing as well as make strategy for continued checking
-  of monitorees. Implement an incremental backoff strategy so that an unavailable
-  host isn't pinged as much after longer unavailability.
-  """
-
+  alias KOTL.Monitor.Supervisor, as: MonSup
+  alias KOTL.Monitor.Manager
   #######
   # API #
   #######
 
-  def start_link(monitorees \\ [], opts \\ [name: __MODULE__]) do
-    GenServer.start_link(__MODULE__, monitorees, opts)
-  end
-
-  def add(monitoree) do
-    add(__MODULE__, monitoree)
-  end
-
-  def add(pid, monitoree) do
-    GenServer.cast(pid, {:add, monitoree})
-  end
-
-  def remove(monitoree) do
-    remove(__MODULE__, monitoree)
-  end
-
-  def remove(pid, monitoree) do
-    GenServer.cast(pid, {:remove, monitoree})
-  end
-
-  def monitorees do
-    monitorees(__MODULE__)
-  end
-
-  def monitorees(pid) do
-    GenServer.call(pid, :monitorees)
+  def monitor(monitoree) do
+    # Code for monitoring here
   end
 
   ############
   # Internal #
   ############
 
-  def handle_call(:monitorees, _from, monitorees) do
-    {:reply, monitorees, monitorees}
-  end
-
-  def handle_cast({:add, monitoree}, monitorees) do
-    {:noreply, [monitoree | monitorees]}
-  end
-
-  def handle_cast({:remove, monitoree}, monitorees) do
-    new_monitorees = Enum.reject(monitorees, &(&1 == monitoree))
-    {:noreply, new_monitorees}
-  end
 end
