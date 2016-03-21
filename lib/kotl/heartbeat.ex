4589d1c4235df4f87a80972e1895a0279459e5c8
Rickard Andersson <gonz@severnatazvezda.com>
2016-03-21 21:45:18 +0200

    Redesign the heartbeat to signify a changed status

--- lib/kotl/heartbeat.ex
+++ lib/kotl/heartbeat.ex
@@ -1,10 +1,10 @@
 defmodule KOTL.Heartbeat do
   @moduledoc"""
-  Stores a datetime and a result. The result is a positive integer representing
-  the time it took to get a response from a server or the like, meaning for
-  example a ping to a hostname to see if it's up or not.
+  Stores a datetime and a status. This should be something like :up/:down.
+
+  This means that we'll store the last change of the status.
   """
   @type t :: %KOTL.Heartbeat{datetime: Timex.DateTime.t,
-                             response: integer}
-  defstruct [:datetime, :response]
+                             status: atom}
+  defstruct [:datetime, :status]
 end
