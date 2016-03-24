24c9437ebf6a2ab5c49083a469b48248b3b66055
Rickard Andersson <gonz@severnatazvezda.com>
2016-03-24 08:10:58 +0200

    Add monitoring for pids

--- lib/kotl/monitor.ex
+++ lib/kotl/monitor.ex
@@ -1,14 +1,22 @@
 defmodule KOTL.Monitor do
-  #######
-  # API #
-  #######
+  alias KOTL.Monitoree
+  alias KOTL.Location
+  alias KOTL.Heartbeat
 
-  def monitor(monitoree) do
-    # Code for monitoring here
+  def monitor(monitoree = %Monitoree{location: loc = %Location{name: name,
+                                                               location: pid,
+                                                               type: :pid},
+                                     changes: changes}) do
+    last_change = List.last(changes)
+    last_status = last_change.status
+    current_status = Process.alive?(pid)
+    if last_status == current_status do
+      current_datetime = Timex.DateTime.universal
+      %Monitoree{location: loc,
+                 changes: changes ++ %Heartbeat{datetime: current_datetime,
+                                                status: current_status}}
+    else
+      monitoree
+    end
   end
-
-  ############
-  # Internal #
-  ############
-
 end
