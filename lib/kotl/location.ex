6470b41e44f4d4a6b0a6a7a750d314f035f5a8d5
Rickard Andersson <gonz@severnatazvezda.com>
2016-03-26 03:27:19 +0200

    Delete obsolete %Location{} and %Monitoree{}

--- lib/kotl/location.ex
+++
@@ -1,6 +0,0 @@
-defmodule KOTL.Location do
-  defstruct [:type, :name, :location]
-  @type t :: %KOTL.Location{type: atom,
-                            name: (atom | String.t),
-                            location: any}
-end
