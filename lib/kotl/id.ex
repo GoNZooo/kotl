bebd63d58e01981555584ed0c08784e48fcda026
Rickard Andersson <gonz@severnatazvezda.com>
2016-03-27 22:32:16 +0300

    Add Inspect implementation for %ID{}

--- lib/kotl/id.ex
+++ lib/kotl/id.ex
@@ -1,4 +1,11 @@
 defmodule KOTL.ID do
   @type t :: %KOTL.ID{type: atom, name: (atom | String.t)}
   defstruct [:type, :name]
+
+  defimpl Inspect, for: KOTL.ID do
+    def inspect(%KOTL.ID{type: type, name: name}, _) do
+      "#ID<#{type}, #{name}>"
+    end
+  end
+
 end
