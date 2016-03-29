63cfae8347935a521330aa28099c15e00f216b7c
Rickard Andersson <gonz@severnatazvezda.com>
2016-03-29 03:40:33 +0300

    Set Spawner to use :interval key from config

--- lib/kotl/monitor/spawner.ex
+++ lib/kotl/monitor/spawner.ex
@@ -2,7 +2,7 @@ defmodule KOTL.Monitor.Spawner do
   alias KOTL.Monitor.Supervisor, as: MonSup
   alias KOTL.Monitor.Manager, as: MonMag
 
-  def start_link(interval \\ 60_000) do
+  def start_link(interval \\ Application.get_env(:kotl, :interval)) do
     Task.start_link(fn -> spawn_loop(interval) end)
   end
 
