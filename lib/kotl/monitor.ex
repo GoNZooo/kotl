b9ad95f396b5d726f57d172d45272ae28965e1d0
Rickard Andersson <gonz@severnatazvezda.com>
2016-03-29 03:38:54 +0300

    Remove heartbeat() implementations
    These are now moved out into a protocol instead and will be defined per
    ID type; added KOTL.ID.{Node,Process}

--- lib/kotl/monitor.ex
+++ lib/kotl/monitor.ex
@@ -1,23 +1,9 @@
 defmodule KOTL.Monitor do
-  alias KOTL.ID
   alias KOTL.Heartbeat
-
-  @spec heartbeat(%{type: :pid, name: (atom | String.t)}) :: Heartbeat.t
-  def heartbeat(id = %ID{type: :pid}) do
-    datetime = Timex.DateTime.universal
-    status = KOTL.NameStore.lookup(id) |> Process.alive?
-    %Heartbeat{datetime: datetime, status: status}
-  end
-
-  @spec heartbeat(%{type: :node, name: (atom | String.t)}) :: Heartbeat.t
-  def heartbeat(id = %ID{type: :node}) do
-    datetime = Timex.DateTime.universal
-    status = KOTL.NameStore.lookup(id) |> Node.ping
-    %Heartbeat{datetime: datetime, status: status}
-  end
+  alias KOTL.Monitor.Check
 
   @spec check(ID.t) :: :ok
   def check(id) do
-    KOTL.Monitor.Manager.add_heartbeat(id, heartbeat(id))
+    KOTL.Monitor.Manager.add_heartbeat(id, Check.heartbeat(id))
   end
 end
