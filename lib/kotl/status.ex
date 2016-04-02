6152a53394d6cbd56690a0103b30263f77358b5c
Rickard Andersson <gonz@severnatazvezda.com>
2016-04-02 14:08:30 +0300

    Add struct to represent status: type, name, status

---
+++ lib/kotl/status.ex
@@ -0,0 +1,17 @@
+defmodule KOTL.Status do
+  defstruct [:type, :name, :status]
+
+  defimpl Inspect, for: KOTL.Status do
+    def inspect(%KOTL.Status{type: type, name: name, status: status}, _) do
+      "#Status<#{type}, #{name}, #{status}>"
+    end
+  end
+
+  #defimpl Poison.Encoder, for: KOTL.Status do
+  #  def encode(status, opts) do
+  #    %{type: status.type,
+  #      name: status.name,
+  #      status: status.status} |> Poison.Encoder.encode(opts)
+  #  end
+  #end
+end
