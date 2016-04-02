c8b5d17d9f094b95007a800de5cc35d84ab831d6
Rickard Andersson <gonz@severnatazvezda.com>
2016-04-02 14:11:44 +0300

    Remove superfluous variable in type_to_atom/1

--- lib/kotl/id/process.ex
+++ lib/kotl/id/process.ex
@@ -15,7 +15,7 @@ defmodule KOTL.ID.Process do
       %KOTL.Heartbeat{datetime: datetime, status: status}
     end
 
-    def type_to_atom(id), do: :process
+    def type_to_atom(_), do: :process
     def up_or_down(_, :pong), do: :up
     def up_or_down(_, _), do: :down
   end
